b0ee3cd1b842aa12460bd25af1e5cf19a45c3f19
B.Maher <bouzid.meher@gmail.com>
2020-01-15 14:55:19 +0100

    rendering of the cards and the pagination done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,48 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { JwPaginationComponent } from "jw-angular-pagination";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HomeComponent } from "./home/home.component";
+import { TestComponent } from "./test/test.component";
+import { CardComponent } from './card/card.component';
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
-
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
+const appRoutes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "test",
+    component: TestComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    TestComponent,
+    JwPaginationComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     MDBBootstrapModule.forRoot(),
-    FormsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
