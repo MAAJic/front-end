0ec84ffff8b054ab0ab59540add053f99cba82c2
jihedabdelly <jihednew7@gmail.com>
2020-01-15 15:14:55 +0100

    static profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CarouselsComponent } from './components/carousels/carousels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProfileComponent,
+    CarouselsComponent
   ],
   imports: [
     BrowserModule,
