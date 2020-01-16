90667d71c042c36ce3a255dafadcca4e970ed861
Ahmed Wheida <ahmed.wheida@gmail.com>
2020-01-16 19:46:45 +0100

    added a fake server

---
+++ server/generate.js
@@ -0,0 +1,15 @@
+var faker = require('faker');
+
+var database = { events: []};
+
+for (var i = 1; i<= 300; i++) {
+  database.events.push({
+    eventId: i,
+    eventCreator: faker.name.firstName() + ' ' + faker.name.lastName(),
+    eventBanner: faker.image.imageUrl(),
+    comments:[{author: faker.name.firstName() + ' ' + faker.name.lastName(), image :faker.image.imageUrl(), content: faker.lorem.paragraph()}, {author: faker.name.firstName() + ' ' + faker.name.lastName(), image :faker.image.imageUrl(), content: faker.lorem.paragraph()}, {author: faker.name.firstName() + ' ' + faker.name.lastName(), image :faker.image.imageUrl(), content: faker.lorem.paragraph()}]
+
+  });
+}
+
+console.log(JSON.stringify(database));
